c62bbdaee1701fcd5421d6e9d2f4b55626ae1f52
Master Yi <yd2277@columbia.edu>
2016-02-10 12:15:53 -0500

    Update myLast.hs

--- docs/myLast.hs
+++ docs/myLast.hs
@@ -3,3 +3,22 @@ myLast :: [a] -> a
 myLast [] = error "Can't get last element on an empty list, dummy!"
 myLast [x] = x
 myLast (_:x) = myLast x
+
+--function composition, using reverse
+myLast :: [a] -> a
+myLast = head . reverse
+
+myLast'' :: [a] -> a
+myLast'' = foldr1 (flip const)
+
+--the use of id
+myLast''' :: [a] -> a
+myLast''' = foldr1 (const id)
+
+--convert snd to curry function
+myLast'''' :: [a] -> a
+myLast'''' = foldr1 (curry snd)
+
+myLast''''' :: [a] -> a
+myLast''''' [] = error "Can't get last element on an empty list, dummy!"
+myLast''''' x = x !! (length x - 1)
